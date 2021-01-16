6cbee28a628859cc740c637e068cf81f46d8b41d
Lukas Krickl <lukas@krickl.dev>
2021-01-16 21:28:44 +0100

    Added notes

--- src/stmt.h
+++ src/stmt.h
@@ -136,6 +136,7 @@ namespace lasm {
             virtual std::any accept(StmtVisitor *visitor);
 
             std::shared_ptr<Token> name;
+            LasmObject value = LasmObject(NIL_O, nullptr);
     };
 
     class InstructionStmt: public Stmt {
